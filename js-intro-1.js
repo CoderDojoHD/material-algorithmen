bce0b3190427dfd24c4418b08d04a0c29ce51b2d
Johannes Link <business@johanneslink.net>
2015-03-10 09:50:53 +0100

    Änderung bei Intro 1

--- js-intro-1.js
+++ js-intro-1.js
@@ -17,10 +17,25 @@ var fuenf = zwei * zwei + 1
 console.log("Hier kommt die", fuenf)
 
 
-// Werte können verändert werden
+// Variablen können verändert werden
 
 zwei = 3
 
 fuenf = fuenf + zwei
 
 console.log("Hier kommt nochmal die", fuenf)
+
+
+// Es gibt auch Listen von Werten, Arrays genannt
+
+var zahlen = [1, 2, 3]
+var summe = zahlen[0] + zahlen[1] + zahlen[2]
+console.log("Die Summe aller Zahlen =", summe)
+
+
+// Auch Arrays können verändert werden
+
+zahlen[0] = 100
+zahlen.push(200)
+console.log("zahlen =", zahlen)
+console.log("100 + 200 =", zahlen[0] + zahlen[3])
