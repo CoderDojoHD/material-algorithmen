e75ba92e0b07a887bd068d0f6cd9d7c982d95dbd
Johannes Link <business@johanneslink.net>
2015-03-10 09:33:30 +0100

    JS Intro 1

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ und ihre Umsetzung in JavaScript beizubringen.
 # Installation
 
 Die Teilnehmer benutzen eine einfache Programmieroberfläche, z.B.
-(repl.it)[http://repl.it/], im Browser.
+[repl.it](http://repl.it/), im Browser.
+
+Das Material kann auch als Repl.It-Session zur Verfügung gestellt werden:
+
+JavaScript-Intro 1: http://repl.it/dVR/2
+
+
+## Quellen für Aufgaben
+
+[Projekt Euler](https://projekteuler.de/)
---
+++ js-intro-1.js
@@ -0,0 +1,26 @@
+// Mit Javascript kann man einfach rechnen und Ausgaben machen
+1 + 1
+
+1 / 3
+
+132 * (5 + 9) / 33.3
+
+console.log("Das Ergebnis lautet:", 100)
+
+
+// Werte kann man sich in Variablen merken
+
+var zwei = 1 + 1
+
+var fuenf = zwei * zwei + 1
+
+console.log("Hier kommt die", fuenf)
+
+
+// Werte können verändert werden
+
+zwei = 3
+
+fuenf = fuenf + zwei
+
+console.log("Hier kommt nochmal die", fuenf)
